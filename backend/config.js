ff6ab12bc8c2062362d8b580a1e08a83d05a27c2
hamzasyed2985 <hamzasyed2985@gmail.com>
2025-05-09 10:52:20 +0500

    Bookstore

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+export const PORT = 5555;
+export const mongoDBURL = 'mongodb://mongo:27017/books-collection';
+
+
+
+
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
